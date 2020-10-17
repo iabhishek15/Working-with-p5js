adb387c56505cac6679f381b752ed84ca0c829e8
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2020-10-17 14:11:16 +0530

    Create sketch.js

---
+++ A*PathFindingAlgo/sketch.js
@@ -0,0 +1,152 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Part 1: https://youtu.be/aKYlikFAV4k
+// Part 2: https://youtu.be/EaZxUCWAjb0
+// Part 3: https://youtu.be/jwRT4PCT6RU
+
+// Function to delete element from the array
+function removeFromArray(arr, elt) {
+    // Could use indexOf here instead to be more efficient
+    for (var i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] == elt) {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+// An educated guess of how far it is between two points
+function heuristic(a, b) {
+    var d = dist(a.i, a.j, b.i, b.j);
+    // var d = abs(a.i - b.i) + abs(a.j - b.j);
+    return d;
+}
+
+// How many columns and rows?
+var cols = 50;
+var rows = 50;
+
+// This will be the 2D array
+var grid = new Array(cols);
+
+// Open and closed set
+var openSet = [];
+var closedSet = [];
+
+// Start and end
+var start;
+var end;
+
+// Width and height of each cell of grid
+var w, h;
+
+// The road taken
+var path = [];
+
+function setup() {
+    createCanvas(400, 400);
+    w = width / cols;
+    h = height / rows;
+
+    for (var i = 0; i < cols; i++) {
+        grid[i] = new Array(rows);
+    }
+
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = new Spot(i, j);
+        }
+    }
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].addNeighbors(grid);
+        }
+    }
+
+    start = grid[0][0];
+    end = grid[cols - 1][rows - 1];
+    start.wall = false;
+    end.wall = false;
+
+    openSet.push(start);
+}
+
+function draw() {
+    if (openSet.length > 0) {
+
+        // Best next option
+        var winner = 0;
+        for (var i = 0; i < openSet.length; i++) {
+            if (openSet[i].f < openSet[winner].f) {
+                winner = i;
+            }
+        }
+        var current = openSet[winner];
+        if (current === end) {
+            noLoop();
+            console.log("DONE!");
+        }
+
+        removeFromArray(openSet, current);
+        closedSet.push(current);
+
+        var neighbors = current.neighbors;
+        for (var i = 0; i < neighbors.length; i++) {
+            var neighbor = neighbors[i];
+            if (!closedSet.includes(neighbor) && !neighbor.wall) {
+                var tempG = current.g + heuristic(neighbor, current);
+                var newPath = false;
+                if (openSet.includes(neighbor)) {
+                    if (tempG < neighbor.g) {
+                        neighbor.g = tempG;
+                        newPath = true;
+                    }
+                } else {
+                    neighbor.g = tempG;
+                    newPath = true;
+                    openSet.push(neighbor);
+                }
+
+                // Yes, it's a better path
+                if (newPath) {
+                    neighbor.h = heuristic(neighbor, end);
+                    neighbor.f = neighbor.g + neighbor.h;
+                    neighbor.previous = current;
+                }
+            }
+
+        }
+    } else {
+        console.log('no solution');
+        noLoop();
+        return;
+    }
+    background(255);
+
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].show();
+        }
+    }
+    for (var i = 0; i < closedSet.length; i++) {
+        closedSet[i].show(color(255, 0, 0, 50));
+    }
+    for (var i = 0; i < openSet.length; i++) {
+        openSet[i].show(color(0, 255, 0, 50));
+    }
+    path = [];
+    var temp = current;
+    path.push(temp);
+    while (temp.previous) {
+        path.push(temp.previous);
+        temp = temp.previous;
+    }
+    noFill();
+    stroke(255, 0, 200);
+    strokeWeight(w / 2);
+    beginShape();
+    for (var i = 0; i < path.length; i++) {
+        vertex(path[i].i * w + w / 2, path[i].j * h + h / 2);
+    }
+    endShape();
+}
